b04d84ab9b8c11458b95b60e21b8bc3754b03edb
Tucker Barrett <tuckerbarrett@gmail.com>
2023-04-01 10:38:32 -0400

    Debugs query issues and updates npm packages

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,10 +2,9 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String
     email: String
-    password: String
     bookCount: Int
     savedBooks: [Book]
   }
--- server/server.js
+++ server/server.js
@@ -16,10 +16,10 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.start()
-.then(() => {
-  server.applyMiddleware({ app });
-});
+// server.start()
+// .then(() => {
+//   server.applyMiddleware({ app });
+// });
 
 
 app.use(express.urlencoded({ extended: false }));
@@ -29,13 +29,28 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
